f12a3e417d38443c0c50289fc0ea2baeb56dcedf
Server <gaurav.bagate@gmail.com>
2025-08-03 21:57:57 +0530

    changes

--- module/ec2/variable.tf
+++ module/ec2/variable.tf
@@ -1,9 +1,34 @@
-variable "instance_type" {
-  description = "The type of instance to create."
+variable "aws_region" {
+  description = "AWS region to deploy into"
   type        = string
 }
 
-variable "aws_region" {
-  description = "The AWS region where resources will be created."
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where instance will be deployed"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "name" {
+  description = "Tag name for the instance"
   type        = string
 }
